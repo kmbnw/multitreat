e6b4194fd48f3e03c44d44154243ece83bd71a20
Krysta Bouzek <nwkrystab@gmail.com>
2016-07-04 15:49:25 -0700

    Converted to a class.

--- src/main/scala/net/kmbnw/multitreat/Prepare.scala
+++ src/main/scala/net/kmbnw/multitreat/Prepare.scala
@@ -18,36 +18,46 @@ package net.kmbnw.multitreat
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.DataFrame
 
-// e.g. designNumeric("funded_amnt", "emp_title", df).show()
-def designNumeric(
-		targetCol: String, groupCol: String, df: DataFrame) : DataFrame = {
+class Multitreat(
+		val targetCol: String,
+		val treatmentCols: Seq[String]) {
 
-	// overall dataframe mean and standard deviation
-	val dfNoNA = df.select(targetCol, groupCol).na.drop()
-	val naFill = 1e-6
-	val sampleMean = dfNoNA.select(avg(targetCol)).first().getDouble(0)
-	val sampleSd = dfNoNA.select(stddev(targetCol)).first().getDouble(0)
+	def designNumeric(df: DataFrame): Map[String, DataFrame] = {
+		return treatmentCols.zip(
+			treatmentCols.map(x => designNumericOnCol(df, x))
+		).toMap
+	}
 
-	// using the simple version of lambda from the paper: lambda = n / (m + n)
-	// where m = group_sd / sample_sd
-	// there is a fill-in for when only one sample exists of 1e-6
-	// TODO I would like to make lambda user-settable
-	val dfMeans = dfNoNA.groupBy(groupCol).agg(
-		avg(targetCol).as("mean"),
-		count(targetCol).divide(
-			stddev(targetCol).divide(sampleSd)
-		    .plus(count(targetCol))).as("lambda")
-	).na.fill(naFill, Array("lambda"))
+	// e.g. designNumeric("funded_amnt", "emp_title", df).show()
+	private def designNumericOnCol(df: DataFrame, groupCol: String): DataFrame = {
 
-	// this is the Bayesian formula:
-	// lambda * group_mean + (1 - lambda) * sample_mean
-	val lambda = dfMeans.col("lambda")
-	val groupMean = dfMeans.col("mean")
-	val ret = dfMeans.withColumn(
-		"S",
-		lambda.multiply(groupMean).plus(
-				lambda.minus(1).multiply(-1 * sampleMean))
-	)
+		// overall dataframe mean and standard deviation
+		val dfNoNA = df.select(targetCol, groupCol).na.drop()
+		val naFill = 1e-6
+		val sampleMean = dfNoNA.select(avg(targetCol)).first().getDouble(0)
+		val sampleSd = dfNoNA.select(stddev(targetCol)).first().getDouble(0)
 
-	return ret
+		// using the simple version of lambda from the paper: lambda = n / (m + n)
+		// where m = group_sd / sample_sd
+		// there is a fill-in for when only one sample exists of 1e-6
+		// TODO I would like to make lambda user-settable
+		val dfMeans = dfNoNA.groupBy(groupCol).agg(
+			avg(targetCol).as("mean"),
+			count(targetCol).divide(
+				stddev(targetCol).divide(sampleSd)
+			    .plus(count(targetCol))).as("lambda")
+		).na.fill(naFill, Array("lambda"))
+
+		// this is the Bayesian formula:
+		// lambda * group_mean + (1 - lambda) * sample_mean
+		val lambda = dfMeans.col("lambda")
+		val groupMean = dfMeans.col("mean")
+		val ret = dfMeans.withColumn(
+			"S",
+			lambda.multiply(groupMean).plus(
+					lambda.minus(1).multiply(-1 * sampleMean))
+		)
+
+		return ret
+	}
 }
